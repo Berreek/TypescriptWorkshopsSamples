91d221b74f6e01b81429eae0f978563866ef2b86
berrek <bartoszbehring@gmail.com>
2018-03-21 22:06:01 +0100

    add generics examples

--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/6 - Generics/genericConstraints.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/6 - Generics/genericConstraints.ts
@@ -1,1 +1,33 @@
-﻿
+﻿interface IExampleInterface {
+    message: string;
+}
+
+class ExampleClass1 implements IExampleInterface {
+    message: string;
+
+    constructor(message: string) { this.message = message; }
+}
+
+class ExampleClass2 {
+    message: string;
+
+    constructor(message: string) { this.message = message; }
+}
+
+class ExampleGenericClass<T extends IExampleInterface> {
+    private readonly value: T;
+
+    constructor(value: T) {
+         this.value = value;
+    }
+
+    public getMessage(): string {
+        return this.value.message;
+    }
+}
+
+function constraintsGenericExample() {
+    let fromClass = new ExampleGenericClass(new ExampleClass1("hello world"));
+    let fromAnonnymousObject = new ExampleGenericClass({ message: "interface hello world" });
+    let fromNotCorrectClass = new ExampleGenericClass(new ExampleClass2("hello world"));
+}
--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/6 - Generics/generics.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/6 - Generics/generics.ts
@@ -1,1 +1,13 @@
-﻿
+﻿class GenericClass<T> {
+    value: T;
+
+    constructor(value: T) {
+         this.value = value;
+    }
+}
+
+function genericsExamples() {
+    let withInt = new GenericClass(3);
+    let withString = new GenericClass("hello world");
+    let withBool = new GenericClass(true);;
+}
