58a8d8438ffdda0700462c39445d091ef87ea7b8
berrek <bartoszbehring@gmail.com>
2018-03-21 21:35:33 +0100

    add enums examples

--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/4 - Enums/enums.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/4 - Enums/enums.ts
@@ -1,1 +1,23 @@
-﻿
+﻿enum SomeEnum {
+    Foo,
+    Bar,
+    X,
+    Y,
+    Z
+}
+
+class SomeClassWithEnum {
+    value: SomeEnum;
+
+    constructor() {
+        this.value = SomeEnum.Bar;
+    }
+}
+
+function enumExample() {
+    let objectWithEnum = new SomeClassWithEnum();
+
+    if (objectWithEnum.value === SomeEnum.Foo) {
+        console.log(objectWithEnum.value);
+    }
+}
