0af10ea2bca36445479d013512ca5634686a2320
berrek <bartoszbehring@gmail.com>
2018-03-21 21:31:26 +0100

    add interfaces

--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/3 - Interfaces/basicInterfaces.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/3 - Interfaces/basicInterfaces.ts
@@ -1,1 +1,39 @@
 ﻿// basic stuff, extending etc readonly
+interface IBasicInterface {
+    message: string;
+}
+
+class BasicClass implements IBasicInterface {
+    message: string;
+
+    constructor(message: string) { this.message = message; }
+}
+
+
+interface IExtendedInterface extends IBasicInterface {
+    otherMessage : string;
+}
+
+class BasicExtendedClass implements IExtendedInterface {
+    otherMessage: string;
+    message: string;
+
+    constructor(otherMessage: string, message: string) {
+        this.otherMessage = otherMessage;
+        this.message = message;
+    }
+}
+
+interface IReadOnlyInterface {
+    readonly message: string;
+}
+
+class ReadOnlyClass implements IReadOnlyInterface {
+    message: string;
+
+    constructor(message: string) { this.message = message; }
+}
+
+function interfaces() {
+    let readonly: IReadOnlyInterface = new ReadOnlyClass("hello world");
+}
--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/3 - Interfaces/interfacesInPractice.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/3 - Interfaces/interfacesInPractice.ts
@@ -1,1 +1,15 @@
-﻿
+﻿interface IInterfaceForTests {
+    message : string;
+}
+
+class TestClass {
+    private readonly message: string;
+
+    constructor(object: IInterfaceForTests) {
+        this.message = object.message;
+    }
+}
+
+function practice() {
+    let testClass = new TestClass({ message: "Hello world" });
+}
