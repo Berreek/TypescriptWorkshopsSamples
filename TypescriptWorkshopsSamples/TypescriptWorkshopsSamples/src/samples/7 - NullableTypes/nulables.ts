b2f4f2099a84fa22169c53e837b8311471f6211d
berrek <bartoszbehring@gmail.com>
2018-03-21 22:19:29 +0100

    add nullables examples

--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/7 - NullableTypes/nulables.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/7 - NullableTypes/nulables.ts
@@ -1,1 +1,25 @@
-﻿
+﻿export class NullableClass {
+    message?: string;
+
+    constructor(message?: string) {
+         this.message = message;
+    }
+}
+
+class ClassWithNullableProperty {
+    nullableObject?: NullableClass;
+
+    constructor(nullableObject?: NullableClass) {
+         this.nullableObject = nullableObject;
+    }
+}
+
+function nullableExample() {
+    let nullableClass = new ClassWithNullableProperty(undefined);
+
+    if (nullableClass.nullableObject) {
+        if (nullableClass.nullableObject.message) {
+            console.log(nullableClass.nullableObject.message);
+        }
+    }
+}
