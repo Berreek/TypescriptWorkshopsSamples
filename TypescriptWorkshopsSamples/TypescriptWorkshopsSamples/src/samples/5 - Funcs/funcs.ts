6504c83ee52b996ee087841c5a094f70c249577c
berrek <bartoszbehring@gmail.com>
2018-03-21 21:57:57 +0100

    added funcs with examples

--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/5 - Funcs/funcs.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/5 - Funcs/funcs.ts
@@ -1,1 +1,22 @@
-﻿
+﻿class ClassWithFunc {
+    public someFunc: (x: number, y: number) => number;
+
+    constructor(someFunc: (x: number, y: number) => number) {
+         this.someFunc = someFunc;
+    }
+}
+
+class ClassWithFuncWithoutStrongType {
+    public someFuncWithoutTyping: (x, y) => any;
+
+    constructor(someFuncWithoutTyping: (x: any, y: any) => any) {
+         this.someFuncWithoutTyping = someFuncWithoutTyping;
+    }
+}
+
+function funcExamples() {
+    let withoutTyping = new ClassWithFuncWithoutStrongType((x, y) => { return x + y; });
+    let withTyping = new ClassWithFunc((x, y) => { return x + y });
+    withoutTyping.someFuncWithoutTyping("hello", "world");
+    withTyping.someFunc(3, 4);
+}
--- TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/5 - Funcs/methods.ts
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/5 - Funcs/methods.ts
@@ -1,1 +1,24 @@
-﻿
+﻿class ClassWithFewMethods {
+    private message: string;
+
+    public simpleMethod(message: string) {
+        this.message = message;
+    }
+
+    public methodWithDefaultValues(message: string, otherMessage = "Foo") {
+        this.message = message;
+    }
+
+    public methodWithParams(message: string, ...messageParams: string[]) {
+        this.message = message;
+    }
+}
+
+function methodsExample() {
+    let classWithMethods = new ClassWithFewMethods();
+
+    classWithMethods.simpleMethod("hello world");
+    classWithMethods.methodWithDefaultValues("hello world");
+    classWithMethods.methodWithDefaultValues("hello world", "other hello world");
+    classWithMethods.methodWithParams("hello world", "hello", "world", "once", "again");
+}
