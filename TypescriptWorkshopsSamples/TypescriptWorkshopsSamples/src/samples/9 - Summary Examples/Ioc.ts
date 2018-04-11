e9235dcf41d6ad65c864c7a63fc0a0546db01bd5
berrek <bartoszbehring@gmail.com>
2018-04-11 18:56:35 +0200

    Add ioc examples

---
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/9 - Summary Examples/Ioc.ts
@@ -0,0 +1,15 @@
+﻿export class IocInTypescript {
+    public registerExample() {
+        //register(ClassToInject);
+    }
+}
+
+export class IocInJavascript {
+    public registerExample() {
+        //register("someDepenendecy", ClassToInject);
+    }
+}
+
+export class ClassToInject {
+    
+}
