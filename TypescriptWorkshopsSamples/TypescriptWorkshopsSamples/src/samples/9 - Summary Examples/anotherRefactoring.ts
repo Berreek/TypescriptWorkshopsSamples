d1a2823584d25dbb94feeae2a5467246e0dd7f32
berrek <bartoszbehring@gmail.com>
2018-04-11 20:02:50 +0200

    Refactoring samples

---
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/9 - Summary Examples/anotherRefactoring.ts
@@ -0,0 +1,11 @@
+﻿import {SomeClassToRefactor} from "./refactoring";
+
+export class AnotherClassToRefactor {
+    public property: SomeClassToRefactor;
+    public anotherProperty: string;
+
+    constructor(property: SomeClassToRefactor) {
+        this.property = property;
+        this.anotherProperty = property.testingProperty;
+    }
+}
---
+++ TypescriptWorkshopsSamples/TypescriptWorkshopsSamples/src/samples/9 - Summary Examples/refactoring.ts
@@ -0,0 +1,3 @@
+﻿export class SomeClassToRefactor {
+    public testingProperty: string;
+}
